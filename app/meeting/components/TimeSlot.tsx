ad2399abf5cf2a8ce45c933e311a31c6a4dd25cc
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-04 23:27:59 +0100

    Selected state in TimeSlot

--- app/meeting/components/TimeSlot.tsx
+++ app/meeting/components/TimeSlot.tsx
@@ -1,5 +1,15 @@
+'use client'
+
+import { useState } from 'react'
+
 export default function TimeSlot({ timeSlot }: { timeSlot: string }) {
+  const [selected, setSelected] = useState(false)
+
+  const handleClick = () => {
+    setSelected(!selected)
+  }
+
   return (
-    <div className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent'>{timeSlot}</div>
+    <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent'>{timeSlot} {selected ? 'selected' : ''}</div>
   )
 }
