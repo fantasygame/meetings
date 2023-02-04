62322b6b7640e4d2c6f91362ce06a5deba893e93
Małgorzata Kubacka <grobelska.m@gmail.com>
2023-02-05 00:00:00 +0100

    Change css on timeSlot select

--- app/meeting/components/TimeSlot.tsx
+++ app/meeting/components/TimeSlot.tsx
@@ -9,7 +9,17 @@ export default function TimeSlot({ timeSlot }: { timeSlot: string }) {
     setSelected(!selected)
   }
 
+  function renderTimeSlotButton(selected: boolean): JSX.Element {
+    if(selected) {
+      return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-blue-700 rounded text-white font-semibold border border-blue-700'>{timeSlot}</div>
+    } else {
+      return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent'>{timeSlot}</div>
+    }
+  }
+
   return (
-    <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent'>{timeSlot} {selected ? 'selected' : ''}</div>
+    <>
+      { renderTimeSlotButton(selected) }
+    </>
   )
 }
