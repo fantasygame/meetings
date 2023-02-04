e2ff51a0a9db6c4d27997b3394cba4dc7140cb7a
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-05 00:22:11 +0100

    Make selected timeslots green

--- app/meeting/components/TimeSlot.tsx
+++ app/meeting/components/TimeSlot.tsx
@@ -16,7 +16,7 @@ export default function TimeSlot({ slot }: { slot: ISlot }) {
 
   function renderTimeSlotButton(selected: boolean): JSX.Element {
     if (selected) {
-      return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-blue-700 rounded text-white font-semibold border border-blue-700 cursor-pointer'>{slot.name}</div>
+      return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-green-700 rounded text-white font-semibold border border-green-700 cursor-pointer'>{slot.name}</div>
     } else {
       return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent cursor-pointer'>{slot.name}</div>
     }
