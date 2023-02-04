a140a82e0e5f989e3ccea4b3e390da28ae527742
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-04 22:15:21 +0100

    Add gap

--- app/meeting/components/Calendar.tsx
+++ app/meeting/components/Calendar.tsx
@@ -5,7 +5,7 @@ export default function Calendar({ meeting }: { meeting: IMeeting }) {
   const dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-7">
+    <div className="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-7 gap-y-6 gap-x-3">
       {dayNames.map((dayName, index) => {
         return <Day dayName={dayName} key={index} />
       })}
