2003b7052c613c5f5a70fe9b8d6553a8898fc1b4
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-05 00:05:41 +0100

    Add cursor pointer

--- app/meeting/components/TimeSlot.tsx
+++ app/meeting/components/TimeSlot.tsx
@@ -8,8 +8,6 @@ export interface ISlot {
 }
 
 export default function TimeSlot({ slot }: { slot: ISlot }) {
-  const timeSlot = slot.name
-
   const [selected, setSelected] = useState(false)
 
   const handleClick = () => {
@@ -17,16 +15,16 @@ export default function TimeSlot({ slot }: { slot: ISlot }) {
   }
 
   function renderTimeSlotButton(selected: boolean): JSX.Element {
-    if(selected) {
-      return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-blue-700 rounded text-white font-semibold border border-blue-700'>{timeSlot}</div>
+    if (selected) {
+      return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-blue-700 rounded text-white font-semibold border border-blue-700 cursor-pointer'>{slot.name}</div>
     } else {
-      return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent'>{timeSlot}</div>
+      return <div onClick={handleClick} className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent cursor-pointer'>{slot.name}</div>
     }
   }
 
   return (
     <>
-      { renderTimeSlotButton(selected) }
+      {renderTimeSlotButton(selected)}
     </>
   )
 }
