b7b19799bcb5780c9e338229202196c76dd2cae6
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-04 22:22:43 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Raleway } from '@next/font/google'
+import Link from 'next/link'
 
 const raleway = Raleway({ subsets: ['latin'] })
 
@@ -7,6 +8,9 @@ export default function Home() {
   return (
     <main className={raleway.className}>
       <h1 className="text-3xl">Hello World</h1>
+      <Link href="/meeting">
+        Miring
+      </Link>
     </main>
   )
 }
