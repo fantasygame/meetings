6c19f3d9cc9aa692cbd3d93adafbb0787e1522c7
Małgorzata Kubacka <grobelska.m@gmail.com>
2023-02-04 23:11:50 +0100

    Improve timeSlot greed to be more responsive

--- app/meeting/components/Day.tsx
+++ app/meeting/components/Day.tsx
@@ -7,7 +7,7 @@ export default function Day({ dayName }: { dayName: string }) {
       </div>
       <div>
         {timeSlots.map((timeSlot, index) => {
-          return <div className='mb-1 text-center w-1/6 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent' key={index}>{timeSlot}</div>
+          return <div className='mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent' key={index}>{timeSlot}</div>
         })}
       </div>
     </div>
