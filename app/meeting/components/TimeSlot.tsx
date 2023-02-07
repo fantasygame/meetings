42e8799b50fa4490b7023bd2c3d77b429560410e
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-07 17:17:24 +0100

    Add text to the button based on availability

--- app/meeting/components/TimeSlot.tsx
+++ app/meeting/components/TimeSlot.tsx
@@ -38,17 +38,21 @@ export default function TimeSlot({ slot, availabilities }: { slot: ISlot, availa
   }
 
   let classNames
+  let buttonText
   if (preferred) {
     classNames = 'mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-yellow-500 rounded text-white font-semibold border border-yellow-500 cursor-pointer'
+    buttonText = ' - Preferuję'
   } else if (selected) {
     classNames = 'mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-green-700 rounded text-white font-semibold border border-green-700 cursor-pointer'
+    buttonText = ' - Mogę grać'
   } else {
     classNames = 'mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent cursor-pointer'
+    buttonText = ''
   }
 
   if (isFetching) {
     classNames += ' opacity-50'
   }
 
-  return <div onClick={handleClick} className={classNames}>{preferred && <>{slot.name} Preferuję</>}{!preferred && <>{slot.name}</>}</div>
+  return <div onClick={handleClick} className={classNames}>{slot.name}{buttonText}</div>
 }
