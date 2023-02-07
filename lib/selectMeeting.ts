463aea7dd6913cb321f5fd64108ad67ffe496dc2
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-07 18:52:13 +0100

    Calculate all promises at once

--- lib/selectMeeting.ts
+++ lib/selectMeeting.ts
@@ -1,12 +1,12 @@
 import findMeetings from '@/lib/findMeetings';
 
 export default async function selectMeeting() {
-  const fiveHourMeetings = await findMeetings(5)
-  const fourHourMeetings = await findMeetings(4)
-  const threeHourMeetings = await findMeetings(3)
+  const fiveHourMeetings = findMeetings(5)
+  const fourHourMeetings = findMeetings(4)
+  const threeHourMeetings = findMeetings(3)
 
-  const meetings = [...fiveHourMeetings, ...fourHourMeetings, ...threeHourMeetings]
-
+  const result = await Promise.all([fiveHourMeetings, fourHourMeetings, threeHourMeetings])
+  const meetings = result.flat()
   // order meetings by number of players and number of time slots (higher number of players first, then higher number of time slots) descenring
   const orderedMeetings = meetings.sort((a, b) => {
     if (b.players.length === a.players.length) {
