6f5e4e47af9f96305e5e68fae4f9deec60116494
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-07 22:02:47 +0100

    Update TimeSlot.tsx

--- app/meeting/components/TimeSlot.tsx
+++ app/meeting/components/TimeSlot.tsx
@@ -48,7 +48,7 @@ export default function TimeSlot({ slot, availabilities }: { slot: ISlot, availa
     buttonText = ' - Preferuję'
   } else if (selected) {
     classNames = 'mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-green-700 rounded text-white font-semibold border border-green-700 cursor-pointer'
-    buttonText = ' - Mogę grać'
+    buttonText = ' - Gram'
   } else {
     classNames = 'mb-1 text-center w-1/3 sm:w-11/12 md:11/12 lg:11/12 px-4 py-2 bg-transparent text-blue-700 rounded hover:bg-blue-700 hover:text-white hover:font-semibold border border-blue-700 hover:border-transparent cursor-pointer'
     buttonText = ''
