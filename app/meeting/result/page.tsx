a94e9b91d7b764004cfcaba41e424d7a5d6e8367
Jakub Kubacki <kubacki.jk@gmail.com>
2023-02-07 22:02:16 +0100

    Update page.tsx

--- app/meeting/result/page.tsx
+++ app/meeting/result/page.tsx
@@ -3,11 +3,15 @@ import { IAvailability } from '@/app/meeting/types/IAvailability'
 import apiUrl from '@/lib/apiUrl'
 import indexToHour from '@/lib/indexToDate'
 import selectMeeting from '@/lib/selectMeeting'
+import Link from 'next/link'
 import { useState, useEffect } from 'react'
 
 export default async function ResultPage() {
   return (
     <div>
+      <Link className='btn ml-5' href="/">
+        Return
+      </Link>
       <AvailableSessions />
     </div>
   )
